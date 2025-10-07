1985094278bdb1ad8a9a53ac4dbb0ae0c3c5245f
NazirHussainTaak <nazirhussaintaak@gmail.com>
2025-10-07 09:57:57 +0400

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,22 @@
+// import { createRoot } from "react-dom/client";
+// import App from "./App.tsx";
+// import "./index.css";
+// import "./i18n/config";
+
+// createRoot(document.getElementById("root")!).render(<App />);
+
+// src/main.tsx
+import React from "react";
 import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 import App from "./App.tsx";
 import "./index.css";
 import "./i18n/config";
 
-createRoot(document.getElementById("root")!).render(<App />);
+createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <BrowserRouter basename="/LionHeart/">
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
+);
